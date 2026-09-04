3f7103e6bbf30efd8baa3b7d257cce5e7ad1feca
Pricillia160424124 <selinsugianto05@gmail.com>
2026-09-04 16:40:43 +0700

    feat : menambah komponen home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,20 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: () => import('./pages/pages.module').then( m => m.PagesPageModule)
   },
+  
+{
+    path:'tabs',
+    children: [
+      { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
+    ]
+},
+
+
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+
 ];
 
 @NgModule({
